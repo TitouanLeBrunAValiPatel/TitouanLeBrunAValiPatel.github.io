3a31ad99cdc0847fd49638efc1b2d8a58b5db1c7
unknown <titouan3.lebrunavalipatel@gmail.com>
2022-08-01 22:43:59 +0200

    mode sun en cours

---
+++ portefolio/js/darkmode.js
@@ -0,0 +1,8 @@
+const mode = document.querySelector('.mode');
+const check = document.querySelector('.check');
+console.log(check);
+console.log(mode);
+
+check.addEventListener('click', () =>{
+    console.log("ca marge");
+});
