bd46ad7360f7ebbd08e271819191476416c79e69
Titouan <titouan3.lebrunavalipatel@gmail.com>
2022-08-11 19:25:45 +0200

    darkmode v2

--- portefolio/js/darkmode.js
+++ portefolio/js/darkmode.js
@@ -2,8 +2,22 @@ const mode = document.querySelector('.mode');
 const check = document.querySelector('.check');
 const bool = document.querySelector('.bool');
 
+console.log(localStorage.getItem("mode"));
+localStorage.getItem("mode") == "false" ? 
+                (
+                    document.body.classList.add('dark-theme'),
+                    bool.classList.add('bool-translate')
+                    )
+                : 
+                (
+                    document.body.classList.remove('dark-theme'),
+                    bool.classList.remove('bool-translate')
+
+                    );
 check.addEventListener('click', () =>{
     bool.classList.toggle('bool-translate');
-    document.body.classList.toggle('dark-theme');
+    localStorage.getItem("mode") == "true" ? (localStorage.setItem("mode","false"), document.body.classList.add('dark-theme'))
+: (localStorage.setItem("mode","true"), document.body.classList.remove('dark-theme'));
+    console.log(localStorage.getItem("mode"));
 
 });
